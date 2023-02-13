8b309f9a684db3414c9a233c7f7017801f0fbc62
Helio <heliojobs25@gmail.com>
2023-02-13 10:58:38 -0300

    feat: sign in, sign register

---
+++ src/pages/login.tsx
@@ -0,0 +1,88 @@
+function login() {
+  return (
+    <section className="gradient-form bg-gray-200 h-full md:h-screen">
+      <div className="container h-full py-12 px-6">
+        <div className="g-6 text-gray-800 flex h-full flex-wrap items-center justify-center">
+          <div className="xl:w-10/12">
+            <div className="bg-white block rounded-lg shadow-lg">
+              <div className="g-0 lg:flex lg:flex-wrap">
+                <div className="px-4 md:px-0 lg:w-6/12">
+                  <div className="md:mx-6 md:p-12">
+                    <div className="text-center">
+                      <img
+                        className="mx-auto w-48"
+                        src="/images/produto-img.svg"
+                        alt="logo"
+                      />
+                      <h4 className="mt-1 mb-12 pb-1 text-xl font-semibold">
+                        We are company name
+                      </h4>
+                    </div>
+                    <form>
+                      <p className="mb-4">Please login to your account</p>
+                      <div className="mb-4">
+                        <input
+                          type="text"
+                          className="form-control text-gray-700 bg-white border-gray-300 focus:text-gray-700 focus:bg-white focus:border-blue-600 m-0 block w-full rounded border border-solid bg-clip-padding px-3 py-1.5 text-base font-normal transition ease-in-out focus:outline-none"
+                          id="exampleFormControlInput1"
+                          placeholder="Username"
+                        />
+                      </div>
+                      <div className="mb-4">
+                        <input
+                          type="password"
+                          className="form-control text-gray-700 bg-white border-gray-300 focus:text-gray-700 focus:bg-white focus:border-blue-600 m-0 block w-full rounded border border-solid bg-clip-padding px-3 py-1.5 text-base font-normal transition ease-in-out focus:outline-none"
+                          id="exampleFormControlInput1"
+                          placeholder="Password"
+                        />
+                      </div>
+                      <div className="mb-12 pt-1 pb-1 text-center">
+                        <button
+                          className="text-white hover:bg-blue-700 mb-3 inline-block w-full rounded px-6 py-2.5 text-xs font-medium uppercase leading-tight shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+                          type="button"
+                          data-mdb-ripple="true"
+                          data-mdb-ripple-color="light"
+                        >
+                          Log in
+                        </button>
+                        <a className="text-gray-500" href="#!">
+                          Forgot password?
+                        </a>
+                      </div>
+                      <div className="flex items-center justify-between pb-6">
+                        <p className="mb-0 mr-2">Don't have an account?</p>
+                        <button
+                          type="button"
+                          className="hover:bg-black inline-block rounded border-2 border-red-600 px-6 py-2 text-xs font-medium uppercase leading-tight text-red-600 transition duration-150 ease-in-out hover:bg-opacity-5 focus:outline-none focus:ring-0"
+                          data-mdb-ripple="true"
+                          data-mdb-ripple-color="light"
+                        >
+                          Danger
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none">
+                  <div className="text-white px-4 py-6 md:mx-6 md:p-12">
+                    <h4 className="mb-6 text-xl font-semibold">
+                      We are more than just a company
+                    </h4>
+                    <p className="text-sm">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default login;
---
+++ src/pages/register.tsx
@@ -0,0 +1,95 @@
+function register() {
+  return (
+    <>
+      <div className="container">
+        <div className="bg-gray-50 flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0">
+          <div>
+            <a href="/">
+              <h3 className="text-purple-600 text-4xl font-bold">Logo</h3>
+            </a>
+          </div>
+          <div className="bg-white mt-6 w-full overflow-hidden px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+            <form>
+              <div>
+                <label
+                  htmlFor="name"
+                  className="text-gray-700 undefined block text-sm font-medium"
+                >
+                  Name
+                </label>
+                <div className="flex flex-col items-start">
+                  <input
+                    type="text"
+                    name="name"
+                    className="border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 mt-1 block w-full rounded-md shadow-sm focus:focus:outline-none focus:ring focus:ring-opacity-50"
+                  />
+                </div>
+              </div>
+              <div className="mt-4">
+                <label
+                  htmlFor="email"
+                  className="text-gray-700 undefined block text-sm font-medium"
+                >
+                  Email
+                </label>
+                <div className="flex flex-col items-start">
+                  <input
+                    type="email"
+                    name="email"
+                    className="border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 mt-1 block w-full rounded-md shadow-sm focus:focus:outline-none focus:ring focus:ring-opacity-50"
+                  />
+                </div>
+              </div>
+              <div className="mt-4">
+                <label
+                  htmlFor="password"
+                  className="text-gray-700 undefined block text-sm font-medium"
+                >
+                  Password
+                </label>
+                <div className="flex flex-col items-start">
+                  <input
+                    type="password"
+                    name="password"
+                    className="border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 mt-1 block w-full rounded-md shadow-sm focus:focus:outline-none focus:ring focus:ring-opacity-50"
+                  />
+                </div>
+              </div>
+              <div className="mt-4">
+                <label
+                  htmlFor="password_confirmation"
+                  className="text-gray-700 undefined block text-sm font-medium"
+                >
+                  Confirm Password
+                </label>
+                <div className="flex flex-col items-start">
+                  <input
+                    type="password"
+                    name="password_confirmation"
+                    className="border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 mt-1 block w-full rounded-md shadow-sm focus:focus:outline-none focus:ring focus:ring-opacity-50"
+                  />
+                </div>
+              </div>
+              <div className="mt-4 flex items-center justify-end">
+                <a
+                  className="text-gray-600 hover:text-gray-900 text-sm underline"
+                  href="#"
+                >
+                  Already registered?
+                </a>
+                <button
+                  type="submit"
+                  className="text-white bg-gray-900 border-transparent active:bg-gray-900 false ml-4 inline-flex items-center rounded-md border px-4 py-2 text-xs font-semibold uppercase tracking-widest transition duration-150 ease-in-out"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default register;
