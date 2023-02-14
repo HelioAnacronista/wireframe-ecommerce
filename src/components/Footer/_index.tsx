b4e241e9d826decf158ff5f1b7559ab9b20ea29d
Helio <heliojobs25@gmail.com>
2023-02-14 19:27:34 -0300

    refactor: footer code optimization and props

--- src/components/Footer/_index.tsx
+++ src/components/Footer/_index.tsx
@@ -1,13 +1,44 @@
-import Networks from "./Networks";
-import Navigation from "./Navigation";
 import Credits from "./Credits";
+import Navigation from "./Navigation";
+import Networks from "./Networks";
+
+interface NavigationProps {
+  companyName: string;
+  email: string;
+  phone: string;
+  address: {
+    street: string;
+    number: number;
+    district: string;
+    city: string;
+    state: string;
+  };
+}
+
+const navProps: NavigationProps = {
+  companyName: "My Company",
+  email: "Company@example.com",
+  phone: "1234567890",
+  address: {
+    street: "123 Main St",
+    number: 456,
+    district: "Downtown",
+    city: "Anytown",
+    state: "NY",
+  },
+};
 
 function Footer() {
   return (
     <footer className="text-gray-600 bg-mainColor-50">
       <div className="container text-center lg:text-left">
         <Networks />
-        <Navigation />
+        <Navigation
+          companyName={navProps.companyName}
+          phone={navProps.phone}
+          email={navProps.email}
+          address={navProps.address}
+        />
         <Credits />
       </div>
     </footer>
